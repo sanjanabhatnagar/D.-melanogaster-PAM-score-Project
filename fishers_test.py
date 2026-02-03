0090d01682d856cac9755d3184e61ceae6040522
Sanjana Bhatnagar <53263995+sanjanabhatnagar@users.noreply.github.com>
2026-02-03 17:29:41 -0500

    Update fishers_test.py

--- fishers_test.py
+++ fishers_test.py
@@ -1,10 +1,11 @@
 import scipy.stats as stats
 
-def calculate_enrichment(overlap_bp, total_peak_bp, pam_cluster_bp, total_genome_bp):
+def calculate_enrichment(overlap_bp, total_peak_bp, pam_group_bp, total_genome_bp):
     # Creating Contingency Table from Data is in Supplementary Table S3
     table = [
-        [overlap_bp, total_peak_bp - overlap_bp],
-        [pam_cluster_bp - overlap_bp, total_genome_bp - total_peak_bp - pam_cluster_bp + overlap_bp]
+        [overlap_bp, total_peak_bp - overlap_bp], # Is a peak-  overlap_bp = Inside a PAM group and (total_peak_bp - overlap_bp) = Outside the PAM group
+        [pam_group_bp - overlap_bp, total_genome_bp - total_peak_bp - pam_group_bp + overlap_bp] # Is not a peak -  pam_cluster_bp - overlap_bp = PAM group no binding and 
+        # (total_genome_bp - total_peak_bp - pam_group_bp + overlap_bp) = the empty background - no peaks
     ]
     odds_ratio, p_value = stats.fisher_exact(table, alternative='greater')
     return p_value
